ef24e6102f924834bb59a48a54e1e3e2803b6fe0
vittesharora <avittesh@gmaill.com>
2019-05-29 10:19:07 +0530

    Basic skeleton created

---
+++ sarcasm/accounts/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+def register(request):
+	return render(request,'register.html',{})
+
+def home(request):
+	return render(request,'home.html',{})	
+
+def leaderboard(request):
+	return render(request,'leaderboard.html',{})
+
+def instructions(request):
+	return render(request,'instructions.html',{})
+
+def forum(request):
+	return render(request,'forum.html',{})
+
+def prize(request):
+	return render(request,'prize.html',{})				
+
